90d7ae9104a5f81ff00ccfeda49ee63718a75187
Yugander <yugandhar@middlewaretalents.com>
2025-05-26 17:04:48 +0530

    new one

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+# Read sourcetype and index from inputs.conf
+SOURCETYPE=$(grep 'sourcetype' inputs.conf | awk -F= '{print $2}' | xargs)
+INDEX=$(grep 'index' inputs.conf | awk -F= '{print $2}' | xargs)
+
 SPLUNK_HEC_URL="$SPLUNK_URL"
 SPLUNK_TOKEN="$SPLUNK_TOKEN"
-INDEX="main"
-SOURCETYPE="harness-demo"
 
 echo "Sending logs to: $SPLUNK_HEC_URL"
+echo "Using sourcetype: $SOURCETYPE"
+echo "Using index: $INDEX"
 
-for log in logs/*.log; do
-  while IFS= read -r line; do
-    curl -s -k "$SPLUNK_HEC_URL/services/collector" \
-      -H "Authorization: Splunk $SPLUNK_TOKEN" \
-      -H "Content-Type: application/json" \
-      -d "{\"event\": \"$line\", \"sourcetype\": \"$SOURCETYPE\", \"index\": \"$INDEX\"}"
-  done < "$log"
-done
+while IFS= read -r line; do
+  curl -s -k "$SPLUNK_HEC_URL/services/collector" \
+    -H "Authorization: Splunk $SPLUNK_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d "{\"event\": \"$line\", \"sourcetype\": \"$SOURCETYPE\", \"index\": \"$INDEX\"}"
+done < log.txt
 
 echo "Deployment finished!"
